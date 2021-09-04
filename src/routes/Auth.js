a931fcb17b4883a54e62da434b6f4f2117dda920
mooooburg-dev <jeongmupark@gmail.com>
2021-09-04 16:37:50 +0900

    setup firebase & router

---
+++ src/routes/Auth.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// export default () => <span>Auth</span>;
+
+// 위의 방식으로 작성해도 되지만
+// Auth 컴포넌트를 사용하는 부모 컴포넌트에서 자동으로 import 되게 하려면
+const Auth = () => <span>Auth</span>;
+export default Auth;
