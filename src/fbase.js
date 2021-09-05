fa6e283282934fc10c7cf789572b46fdc6885fc9
mooooburg-dev <jeongmupark@gmail.com>
2021-09-06 07:52:09 +0900

    Authentication

---
+++ src/fbase.js
@@ -0,0 +1,20 @@
+// import * as firebase from "firebase/app";
+// import "firebase/auth";
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// firebase.initializeApp(firebaseConfig);
+// export const authService = firebase.auth();
+
+const app = initializeApp(firebaseConfig);
+export const authService = getAuth();
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,56 @@
-import React from "react";
+import { EmailAuthCredential } from "@firebase/auth";
+import React, { useState } from "react";
 
 // export default () => <span>Auth</span>;
 
 // 위의 방식으로 작성해도 되지만
 // Auth 컴포넌트를 사용하는 부모 컴포넌트에서 자동으로 import 되게 하려면
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Login" />
+      </form>
+
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </>
+  );
+};
 export default Auth;
