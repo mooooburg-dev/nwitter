38b70fdff518bec1ed2ee3945be9b8be099f4a4c
mooooburg-dev <jeongmupark@gmail.com>
2021-09-15 10:35:53 +0900

    FileUpload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from "react";
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     onSnapshot(collection(dbService, "nweets"), (snapshot) => {
@@ -38,6 +39,22 @@ const Home = ({ userObj }) => {
     setNweet(value);
   };
 
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+
+    const theFile = files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = ({ currentTarget: { result } }) => {
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearAttachment = () => setAttachment(null);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -48,7 +65,14 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         ></input>
-        <input type="submit" value="Nweet"></input>
+        <input type="file" accept="image/*" onChange={onFileChange} />
+        <input type="submit" value="Nweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
