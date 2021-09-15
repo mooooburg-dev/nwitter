a3ca97e9846562ebfd81dc31031912803afeea72
mooooburg-dev <jeongmupark@gmail.com>
2021-09-15 10:35:42 +0900

    delete footer

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ function App() {
       ) : (
         "Initializing"
       )}
-      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+      {/* <footer>&copy; {new Date().getFullYear()} Nwitter</footer> */}
     </>
   );
 }
