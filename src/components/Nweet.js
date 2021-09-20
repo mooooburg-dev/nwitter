5f8bfb41b7ee8107270936c9a2a8b61063a7f854
mooooburg-dev <jeongmupark@gmail.com>
2021-09-20 17:21:07 +0900

    warning 제거

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -53,7 +53,9 @@ const Nweet = ({ nweetObj, isOwner }) => {
       ) : (
         <>
           <h4>{nweetObj.text}</h4>
-          {nweetObj.attachmentUrl && <img src={nweetObj.attachmentUrl} />}
+          {nweetObj.attachmentUrl && (
+            <img src={nweetObj.attachmentUrl} alt="attachment" />
+          )}
           {isOwner && (
             <div className="nweet__actions">
               <span onClick={onDeleteClick}>
